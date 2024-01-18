2d2784aa6620e3613b314c536ddac350a56132be
hamzas7 <hamza.yasr7@gmail.com>
2024-01-18 13:01:22 -0800

    do nothing

--- main.h
+++ main.h
@@ -73,4 +73,6 @@ void f_stack(stack_t **head, unsigned int counter);
 void f_queue(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
 void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+
 #endif
